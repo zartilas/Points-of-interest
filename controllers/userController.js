5aa1d8639b834427b0626db190d3edd6e8997f2e
zartilas <zartilasvasilios@protonmail.com>
2022-02-21 12:40:10 +0200

    Created Project

---
+++ controllers/userController.js
@@ -0,0 +1,90 @@
+const {User, validate} = require('../models/user');
+
+const getAllUsers = async (req, res, next) => {
+    const list = await User.find().exec();
+    res.render('userlist', {
+        users: list
+    });
+}
+
+const getAddUserView = (req, res, next) => {
+    res.render('addUser');
+}
+
+const addUser = async (req, res, next) => {
+    const {error} = validate(req.body);
+    if(error) return res.status(422).send(error.details[0].message);
+    const data = req.body;
+    let user = await new User({
+        firstname: data.firstname,
+        lastname: data.lastname,
+        phonenumber: data.phonenumber,
+        cnic: data.cnic,
+        email: data.email
+    });
+    user = await user.save();
+    res.redirect('/');
+}
+
+const getUpdateUserView = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const oneuser = await User.findById(id).exec();
+        res.render('updateUser', {
+            user: oneuser
+        });
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const updateUser = async(req, res, next) => {
+    const {error} = validate(req.body);
+    if (error) return res.status(422).send(error.details[0].message);
+    const id = req.params.id;
+    const data = req.body;
+    let user = await User.findByIdAndUpdate(id, {
+        firstname: data.firstname,
+        lastname: data.lastname,
+        phonenumber: data.phonenumber,
+        cnic: data.cnic,
+        email: data.email
+    }, {new: true});
+    if(!user) return res.status(404).send('User with the given id not found');
+
+    res.redirect('/');
+}
+
+const getDeleteUserView = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const oneuser = await User.findById(id).exec();
+        res.render('deleteUser', {
+            user: oneuser
+        });
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const deleteUser = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await User.findByIdAndRemove(id);
+        if(!user) return res.status(404).send('User with the given id not found');
+        res.redirect('/');        
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+
+module.exports = {
+    getAllUsers,
+    getAddUserView,
+    addUser,
+    getUpdateUserView,
+    updateUser,
+    getDeleteUserView,
+    deleteUser
+}
---
+++ models/users.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+require('mongoose-type-email');
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        minlength: 5,
+        maxlength: 50,
+        required: true
+    },
+    lastname: {
+        type: String,
+        minlength: 5,
+        maxlength: 50,
+        required: true
+    }, 
+    phonenumber: {
+        type: String,
+        minlength: 11,
+        required: true
+    },
+    cnic: {
+        type: String,
+        required: true
+    }, 
+    email: {
+       type: mongoose.SchemaTypes.Email, 
+       lowercase: true,
+       minlength: 5,
+       maxlength: 50,
+       required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+const validateUser = (user) => {
+    const schema = {
+        firstname: Joi.string().min(5).max(50).required(),
+        lastname: Joi.string().min(5).max(50).required(),
+        phonenumber: Joi.string().min(11).required(),
+        cnic: Joi.string().required(),
+        email: Joi.email().min(5).max(50).required()
+    }
+
+    return Joi.validate(user, schema);
+}
+
+
+module.exports.User = User;
+module.exports.validate = validateUser;
